0795c93bf5f1f637125193dc2243c83ddccb4357
caioamisterdam <caioamisterdam@gmail.com>
2022-12-01 17:49:51 -0300

    update

--- Desafios/lab-padroes-projeto-java/src/one/digitalinnovation/gof/facade/Facade.java
+++ Desafios/lab-padroes-projeto-java/src/one/digitalinnovation/gof/facade/Facade.java
@@ -1,2 +1,14 @@
-package one.digitalinnovation.gof.facade;public class Facade {
+package one.digitalinnovation.gof.facade;
+
+import one.digitalinnovation.gof.subsistema1.crm.CrmService;
+import one.digitalinnovation.gof.subsistema2.cep.CepAPI;
+
+public class Facade {
+
+    public void migrarCliente(String nome, String cep) {
+        String cidade = CepAPI.getInstancia().recuperarCidade(cep);
+        String estado = CepAPI.getInstancia().recuperarEstado(cep);
+
+        CrmService.gravarCliente(nome, cep, cidade, estado);
+    }
 }
--- Desafios/lab-padroes-projeto-java/src/one/digitalinnovation/gof/subsistema2/cep/CepAPI.java
+++ Desafios/lab-padroes-projeto-java/src/one/digitalinnovation/gof/subsistema2/cep/CepAPI.java
@@ -1,2 +1,24 @@
-package one.digitalinnovation.gof.subsistema2.cep;public class CepAPI {
+package one.digitalinnovation.gof.subsistema2.cep;
+
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+
+public class CepAPI {
+
+    private static CepAPI instancia = new CepAPI();
+
+    private CepAPI() {
+        super();
+    }
+
+    public static CepAPI getInstancia() {
+        return instancia;
+    }
+
+    public String recuperarCidade(String cep) {
+        return "Araraquara";
+    }
+
+    public String recuperarEstado(String estado) {
+        return "SP";
+    }
 }
